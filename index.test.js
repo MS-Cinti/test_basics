16aca7b76cc3fb9083b8bfe37736a5446e5c5dfa
MS-Cinti <szabadicintia@gmail.com>
2022-04-22 08:29:08 +0200

    added space

--- index.test.js
+++ index.test.js
@@ -12,6 +12,7 @@ it("Search with empty result.", () => {
     expect(search(mockDb, "zzz")).toEqual([])
 })
 
+
 it("Real search with cool keyword, expected non-empty array as a result.", () => {
     expect(search(mockDb, "cool")).toEqual(["codecool2", "cool cat2", "frontend is so cool2", "cool school2", "cool day2"])
 })
